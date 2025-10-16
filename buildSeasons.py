f3a731bdf06f2451f05ae2ffe7ebfaa3366b6261
Kyle Minor <kyle.s.minor.85@gmail.com>
2025-10-16 10:16:54 -0400

    Added Random Walker Rankings method, with suitable test cases

--- buildSeasons.py
+++ buildSeasons.py
@@ -5,6 +5,7 @@ Created on Tue Oct 14 15:36:03 2025
 @author: hokie
 """
 
+from copy import deepcopy
 import pandas as pd
 import numpy as np
 
@@ -24,13 +25,13 @@ class Season:
     perfTeams = pd.DataFrame({"TeamID":[1,2,3],
                               "TeamName":["A","B","C"]})
     
-    def __init__(self,seasonType, seasonID=-1, subsetID=-1):
+    def __init__(self,seasonType, seasonID=-1, subsetID=-1, rwrP = 0.75):
         self.seasonType = str(seasonType)
         self.getData(seasonID,subsetID)
-        self.tbls = self.__makeAdjacency__()
+        self.tbls = self.__makeAdjacency__(rwrP)
         
     def getData(self,seasonID, subsetID):
-        if (seasonID > 0 & subsetID > 0):
+        if (seasonID > 0 and subsetID > 0):
             self.season = pd.read_csv(Season.urlHook+"s="+str(seasonID)+
                                  "&sub="+str(subsetID)+"&all=1&mode=3&sch=on&format=1",
                                 header=None,
@@ -45,13 +46,13 @@ class Season:
             self.season = self.perfSeason
             self.teams = self.perfTeams
         
-    def __makeAdjacency__(self):
-        C = np.zeros(shape=(self.teams.shape[0],self.teams.shape[0]))
+    def __makeAdjacency__(self,rwrP):
+        MC_C = np.zeros(shape=(self.teams.shape[0],self.teams.shape[0]))
+        NP_C = deepcopy(MC_C) # Adjacency for Neumann-Park 
+        RWR_N = deepcopy(MC_C) # Adjacency for RWR - games only (existence)
+        RWR_B = deepcopy(MC_C)
         WL = np.zeros(shape=(self.teams.shape[0],2))
         PTS = np.zeros(shape=(self.teams.shape[0],2))
-        
-        MC_C = C # Adjacency for Colley and Massey
-        NP_C = C # Adjacency for Neumann-Park
         
         for gm in self.season.itertuples():
             tms = (int(gm.Team1)-1,int(gm.Team2)-1)
@@ -59,9 +60,11 @@ class Season:
             
             for i in range(2):
                 WL[tms[i]] += (1,0) if (scores[i]>scores[(i+1)%2]) else (0,1)
+                RWR_B[tms[i],tms[(i+1)%2]] += 1 if (scores[i]>scores[(i+1)%2]) else -1
                 for j in range(2):
                     MC_C[tms[i],tms[j]] += 1 if (i == j) else -1
-                    NP_C[tms[i],tms[j]] += 1 if ((i != j) and (scores[j] > scores[i])) else 0
+                    NP_C[tms[i],tms[j]] += 1 if ((i != j) and (scores[i] > scores[j])) else 0
+                    RWR_N[tms[i],tms[j]] += 1 if (i != j) else 0
                     PTS[tms[i],j] += scores[0] if (i == j) else scores[1]
                     
         GmsTbl = pd.DataFrame({'Played' : np.diag(MC_C)}).drop_duplicates().sort_values('Played')
@@ -72,7 +75,8 @@ class Season:
         colTbls = self.makeColley(MC_C,WL)
         masTbls = self.makeMassey(MC_C,PTS)
         neuTbls = self.makeNeumann(NP_C,GmsTbl)
-        return(colTbls,masTbls)
+        rwrTbls = self.makeRwr(RWR_N,RWR_B,WL,rwrP)
+        return(colTbls,masTbls,neuTbls,rwrTbls)
 
     def makeColley(self,C,WL):
         C_Col = C + 2*np.eye(self.teams.shape[0]) # Add 2 to each diagonal element
@@ -81,7 +85,7 @@ class Season:
     
     def makeMassey(self,C,PTS):
         b = PTS[:,0]-PTS[:,1]
-        C_Mas = C
+        C_Mas = deepcopy(C)
         
         # Fix the rank deficiency by setting the last row of C to 1,
         # and the corresponding entry in b to 0.
@@ -90,28 +94,36 @@ class Season:
         return(C_Mas,b)
     
     def makeNeumann(self, C, GMS):
-        C_NP = C
-        if (max(np.linalg.eigvals(C)) == 0):
-            return(0)
-        else:
-            alpha = self.getAlpha(GMS)
-        
+        C_NP = deepcopy(C)
+        alpha = 0 if (max(np.linalg.eigvals(C)) == 0) else self.__getAlpha__(GMS)
+        return(C_NP,alpha)
     
-    def makeRwr():
-        pass
+    def makeRwr(self,N,B,WL,p):
+        D = np.zeros(shape=B.shape)
+
+        for k in range(D.shape[0]):
+            D[k,k] = ((-p)*WL[k,1]) - ((1-p)*WL[k,0])
+            for j in range(D.shape[1]):
+                D[k,j] = ((0.5 * N[k,j])) + (((2*p)-1)/2)*B[k,j] if (j != k) else D[k,j]
+                
+        return(D)
+                    
     
-    def getAlpha(self,M):
+    def __getAlpha__(self,M):
+        return(1/4.875)
         
-        tot = sum(M.Count) # Probability Divisor
-        num = 0
-        denom = 0
-        ct = 1
-        for i in M.itertuples():
-            p = i.Count/tot
-            num += p*ct*(ct-1)
-            denom += p*ct
-            ct += 1
-        return((2*denom)/num)
+# =============================================================================
+#         FIX THIS STUFF
+#         # Mean games played - count of games divided by count of teams
+#         mean = (self.season.len * 2) / (len(self.teams.len))
+#         
+#         # MeanSquare - (actual)^2 divided by (teams-1)
+#         # In this case, we'll weight each group of observations by the count
+#         meanSquare = sum(M['Count']*(M['Played'])**2) / (len(self.teams)-1)
+#         
+#         return ((2*mean) / (meanSquare - mean))
+# =============================================================================
+        
         
 MBB25 = Season("Men's Basketball",604302,11590)
 
--- test_buildSeasons.py
+++ test_buildSeasons.py
@@ -6,13 +6,14 @@ Created on Wed Oct 15 10:26:40 2025
 """
 
 from buildSeasons import Season
-from solvers import LinearSolver
+from solvers import Solvers
 import unittest
 import numpy as np
 
 
 class seasonTests(unittest.TestCase):
-    pSeason = Season("Test")
+    pSeason = Season("Test",rwrP = 0.75)
+    sol = Solvers()
 
     def testColley(self):
         perfC = np.array(([4,-1,-1],[-1,4,-1],[-1,-1,4])).astype(float)
@@ -21,10 +22,10 @@ class seasonTests(unittest.TestCase):
         perfB = np.array(([2,1,0])).astype(float)
         self.assertTrue(np.array_equal(perfB, self.pSeason.tbls[0][1]))
         
-        perfSol = LinearSolver(perfC,perfB)
+        perfSol = self.sol.LinearSolver(C=perfC,b=perfB)
         self.assertTrue(np.array_equal(perfSol,
-                         LinearSolver(self.pSeason.tbls[0][0], 
-                                      self.pSeason.tbls[0][1])))
+                         self.sol.LinearSolver(C=self.pSeason.tbls[0][0], 
+                                      b=self.pSeason.tbls[0][1])))
         
     def testMassey(self):
         perfC = np.array(([2,-1,-1],[-1,2,-1],[1,1,1])).astype(float)
@@ -33,12 +34,30 @@ class seasonTests(unittest.TestCase):
         perfB = np.array(([3,0,0])).astype(float)
         self.assertTrue(np.array_equal(perfB, self.pSeason.tbls[1][1]))
         
-        perfSol = LinearSolver(perfC,perfB)
+        perfSol = self.sol.LinearSolver(C=perfC,b=perfB)
         self.assertTrue(np.array_equal(perfSol,
-                         LinearSolver(self.pSeason.tbls[1][0], 
-                                      self.pSeason.tbls[1][1])))
+                         self.sol.LinearSolver(C=self.pSeason.tbls[1][0], 
+                                      b=self.pSeason.tbls[1][1])))
         
-
+# =============================================================================
+#     def testNeumann(self):
+#         perfC = np.array(([0,1,1],[0,0,1],[0,0,0]))
+# =============================================================================
+        
+    def testRWR(self):
+        perfD = np.array(([-0.5,0.75,0.75],[0.25,-1,0.75],[0.25,0.25,-1.5]))
+        self.assertTrue(np.array_equal(perfD,self.pSeason.tbls[3]))
+        
+        perfSol = self.sol.RWRSolver(D=perfD)
+        self.assertTrue(np.array_equal(perfSol,
+                         self.sol.RWRSolver(D=self.pSeason.tbls[3])))
+        
+        tSeason = Season("Test",rwrP = 0.6)
+        testD = np.array(([-0.8,0.6,0.6],[0.4,-1,0.6],[0.4,0.4,-1.2]))
+        testSol = self.sol.RWRSolver(D=testD)
+        self.assertTrue(np.array_equal(testSol,
+                         self.sol.RWRSolver(D=tSeason.tbls[3])))
+        
 sTest = seasonTests()
 unittest.main()
 # =============================================================================
